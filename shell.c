d731fb6c7cb9033bf86d342f2752b5761176e2ea
Oluwaseyi Adeosun <deosundeola@gmail.com>
2023-04-19 15:50:26 -0700

    main_function

---
+++ shell.c
@@ -0,0 +1,68 @@
+#include "shell.h"
+
+/**
+ * main - start of function.
+ * @argc: argument count.
+ * @argv: array of string pointers containing command line arguments.
+ *
+ * Return: int.
+ */
+int main (int argc, char **argv)
+{
+	char *prompt = "simple_shell$ ";
+	char *markptr = NULL, *markptr_dup = NULL;
+	size_t size = 0;
+	ssize_t str;
+	const char *dim = " \n";
+	int count, tok_count = 0;
+	char *tok;
+	(void)argc;
+
+
+	while (1)
+	{
+		write(STDOUT_FILENO, prompt, strlen(prompt));
+
+		str = getline(&markptr, &size, stdin);
+		if (str == -1)
+		{
+			write(STDOUT_FILENO, "Exiting shell....\n", strlen("Exiting shell....\n"));
+			return (-1);
+		}
+
+		markptr_dup = malloc(sizeof(char) * str);
+		if (markptr == NULL)
+		{
+			perror("tsh: memory not allocated");
+			return (-1);
+		}
+		strcpy(markptr_dup, markptr);
+
+		tok = strtok(markptr, dim);
+
+		while (tok != NULL)
+		{
+			tok_count++;
+			tok = strtok(NULL, dim);
+		}
+
+		argv = malloc(sizeof(char *) * (tok_count + 1));
+
+		tok = strtok(markptr_dup, dim);
+
+		for (count = 0; tok != NULL; count++)
+		{
+			argv[count] = malloc(sizeof(char) * strlen(tok));
+			strcpy(argv[count], tok);
+			tok = strtok(NULL, dim);
+		}
+		argv[count] = NULL;
+
+		execute_command(argv);
+	}
+	free(markptr);
+	free(markptr_dup);
+
+
+	return (0);
+ }
