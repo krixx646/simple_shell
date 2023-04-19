60d012c3e904cb6bc63fc854369c4881bba60c11
Oluwaseyi Adeosun <deosundeola@gmail.com>
2023-04-19 15:54:25 -0700

    execmd_file

---
+++ execmd.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+
+/**
+ * execute_command -  execute the file.
+ * @argv: a char double pointer.
+ *
+ * Return: no return.
+ */
+void execute_command(char **cmd)
+{
+	char *exe = NULL;
+
+	if(cmd)
+	{
+		exe = cmd[0];
+		if (execve(exe, cmd, NULL) == -1)
+		{
+			perror("Error:");
+		}
+	}
+}
