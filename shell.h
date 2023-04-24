d636d4af9123a47b678473d1a6268002db326822
Agba Valentine Cheluchi <krixxvalle@gmail.com>
2023-04-24 17:41:29 +0000

    header

--- shell.h
+++ shell.h
@@ -5,7 +5,31 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stddef.h>
 
-void execute_command(char **argv, int var);
+/* Macros unit Variables*/
+
+#include MAX_NUM 20;
+#include UNUD_A 10;
+
+/* String header files */
+
+void print_prompt(char *prompt);
+char *read_input();
+int parse_input(char *input, char **argv);
+char *find_command(char *command);
+void execute_command(char *command, char **argv, char **environ);
+
+char *strchr(const char *str, int c);
+size_t strlen(const char *str);
+char *getenv(const char *name);
+
+/* Function Headers will be inserted here */
 
 #endif
